6d8aa59612702a29585077c8aaa8609c611e1a85
iquidus <luke@iquidus.io>
2021-02-09 03:15:57 +0100

    deps: add graphql, apollo; tokens: fetch from shinobi

--- components/tables/tokensTable.vue
+++ components/tables/tokensTable.vue
@@ -1,16 +1,41 @@
 <template>
-  <table-view no-loading :headers="headers" :items="tokens" item-key="address">
-    <template v-slot:item.address="{ value: address }">
-      <nuxt-link
-        :to="{ name: 'account-address', params: { address: address } }"
-      >
-        {{ address.substr(0, 23) }}...
-      </nuxt-link>
+  <table-view no-loading :headers="headers" :items="tokens" item-key="id">
+    <template v-slot:item.name="{ item }">
+      <v-avatar size="24">
+        <v-img
+          :src="
+            'https://raw.githubusercontent.com/octanolabs/assets/master/blockchains/ubiq/assets/' +
+            checksumHash(item.id) +
+            '/logo.png'
+          "
+        />
+      </v-avatar>
+      <template v-if="item.symbol !== 'WUBQ'">
+        <nuxt-link
+          :to="{ name: 'account-address', params: { address: item.id } }"
+        >
+          {{ item.name }}
+        </nuxt-link>
+      </template>
+      <template v-if="item.symbol === 'WUBQ'">Ubiq</template>
+    </template>
+    <template v-slot:item.symbol="{ item }">
+      <template v-if="item.symbol !== 'WUBQ'">
+        {{ item.symbol }}
+      </template>
+      <template v-if="item.symbol === 'WUBQ'">UBQ</template>
+    </template>
+    <template v-slot:item.derivedETH="{ value: derivedUBQ }">
+      ${{ (derivedUBQ * ubqPrice).toFixed(4) }}
+    </template>
+    <template v-slot:item.totalLiquidity="{ item }">
+      ${{ (item.totalLiquidity * (item.derivedETH * ubqPrice)).toFixed(2) }}
     </template>
   </table-view>
 </template>
 
 <script>
+import { toChecksumAddress } from 'ethereumjs-util'
 import tableView from '~/components/util/TableView'
 export default {
   components: {
@@ -22,28 +47,40 @@ export default {
       required: true,
       default: () => [],
     },
+    ubqPrice: {
+      type: Number,
+      required: true,
+      default() {
+        return 0
+      },
+    },
   },
   data() {
     return {
       headers: [
         {
-          value: 'symbol',
-          text: 'Symbol',
-        },
-        {
           value: 'name',
           text: 'Name',
         },
         {
-          value: 'decimals',
-          text: 'Decimals',
+          value: 'symbol',
+          text: 'Symbol',
         },
         {
-          value: 'address',
-          text: 'Address',
+          value: 'derivedETH',
+          text: 'Value (USD)',
+        },
+        {
+          value: 'totalLiquidity',
+          text: 'Liquidity (USD)',
         },
       ],
     }
+  },
+  methods: {
+    checksumHash(hash) {
+      return toChecksumAddress(hash)
+    },
   },
 }
 </script>
--- pages/tokens.vue
+++ pages/tokens.vue
@@ -1,36 +1,101 @@
 <template>
   <v-col cols="12">
-    <tokens-table :tokens="getTokens()" />
+    <tokens-table :tokens="tokens" :ubq-price="price" />
   </v-col>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
 import tokensTable from '~/components/tables/tokensTable'
-import tokens from '~/scripts/tokens'
 
 export default {
   components: {
     tokensTable,
   },
+  data() {
+    return {
+      tokens: [],
+      price: 0,
+    }
+  },
+  async created() {
+    this.tokens = await this.getAllTokensOnShinobi()
+  },
   methods: {
-    getTokens() {
-      const tks = tokens.getTokens()
-      const result = []
-
-      for (const address of Object.keys(tks)) {
-        if (tks[address].display) {
-          const token = {
-            name: tks[address].name,
-            symbol: tks[address].symbol,
-            decimals: tks[address].decimals,
-            address,
+    async getAllTokensOnShinobi() {
+      const TOKENS_TO_FETCH = 100
+      const UBQ_PRICE = gql`
+        query bundles {
+          bundles(where: { id: 1 }) {
+            id
+            ethPrice
           }
-
-          result.push(token)
         }
+      `
+      const ALL_TOKENS = gql`
+        query tokens($skip: Int!) {
+          tokens(
+            first: 500
+            skip: $skip
+            orderBy: tradeVolumeUSD
+            orderDirection: desc
+          ) {
+            id
+            name
+            symbol
+            derivedETH
+            tradeVolume
+            tradeVolumeUSD
+            untrackedVolumeUSD
+            totalLiquidity
+            txCount
+          }
+        }
+      `
+      const client = new ApolloClient({
+        link: new HttpLink({
+          uri: 'https://graphnode.octano.dev/subgraphs/name/octanolabs/shinobi',
+        }),
+        cache: new InMemoryCache(),
+        shouldBatch: true,
+      })
+      try {
+        let allFound = false
+        let skipCount = 0
+        let tokens = []
+        while (!allFound) {
+          const price = await client.query({
+            query: UBQ_PRICE,
+            variables: {
+              skip: skipCount,
+            },
+            fetchPolicy: 'cache-first',
+          })
+          const result = await client.query({
+            query: ALL_TOKENS,
+            variables: {
+              skip: skipCount,
+            },
+            fetchPolicy: 'cache-first',
+          })
+          tokens = tokens.concat(result?.data?.tokens)
+          if (
+            result?.data?.tokens?.length < TOKENS_TO_FETCH ||
+            tokens.length > TOKENS_TO_FETCH
+          ) {
+            allFound = true
+          }
+          skipCount = skipCount += TOKENS_TO_FETCH
+          this.price = price.data.bundles[0].ethPrice
+        }
+        console.log(tokens)
+        return tokens
+      } catch (e) {
+        console.log(e)
       }
-
-      return result
     },
   },
 }
